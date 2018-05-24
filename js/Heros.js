23f3337624c75cc5f1aab7dc57f7a304b8c4dcb6
Alexandre Ba <alexandre64.ba@gmail.com>
2018-05-25 00:08:15 +0200

    test direction

--- js/Heros.js
+++ js/Heros.js
@@ -25,7 +25,43 @@ class Heros extends Humanoide{
             case 83: // Bas
             this.y += 10;
             break;
-        };
+        };/*
+        switch(e.keyCode){
+            case 38:
+           // case 90: // Haut
+            this.up = true;
+            break;
+            case 39:
+          //  case 68: // Droit 
+            this.right = true;
+            break;
+            case 37:
+           // case 81: // Gauche
+            this.left = true;
+            break;
+            case 40:
+          //  case 83: // Bas
+            this.down = true;
+            break;
+         };
+        switch(this.left, this.up, this.right, this.down) {
+            case this.up == true:this.y -= 10;
+            break;
+            case this.down == true:this.y +=10;
+            break;
+            case this.left == true:this.x -= 10;
+            break;
+            case this.right == true:this.x +=10;
+            break;
+            case this.up== true && this.left == true: this.y -= 10, this.x -= 10;
+            break;
+            case this.up== true && this.right == true: this.y -= 10, this.x += 10;
+            break;
+            case this.down== true && this.left == true: this.y += 10, this.x -= 10;
+            break;
+            case this.down== true && this.right == true: this.y += 10, this.x += 10;
+            break;
+        }*/
     };
     
 };
--- js/init.js
+++ js/init.js
@@ -12,6 +12,9 @@ addEventListener("keydown", function(e){
     mecanique.heros.deplacement(e);//(e.keycode, true)
 })
 
+addEventListener("keyup", function(e){
+    mecanique.heros.deplacement(e);//(e.keycode, false)
+})
 
 
 
