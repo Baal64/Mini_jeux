e1c33cfd14a235b511cfb034bc4056410a5bbf7a
Alexandre Ba <alexandre64.ba@gmail.com>
2018-05-24 23:20:20 +0200

    cool

--- js/Mecanique.js
+++ js/Mecanique.js
@@ -6,17 +6,20 @@ class Mecanique{
         this.monstre = new Monstre ("img/monster.png", this.canvas.context);
         this.heros   = new Heros ("img/hero.png", this.canvas.context);
         this.bombs = [];
-        console.log(this.bombs);
         this.creaBombes();
+        console.log(this.bombs);
     }
 
     update(){
         this.canvas.context.clearRect(0, 0, this.l, this.h);
         this.plateau.dessinePlateau();
-       // this.bombe.dessineHuma();
+        for(let i =0; i<this.bombs.length; i++){
+            this.bombs[i].dessineHuma();
+        };
         this.monstre.dessineHuma();
         this.heros.dessineHuma();
         this.collisions();
+        this.collisionsBombs();
     }
 
     collisions(){
@@ -26,20 +29,25 @@ class Mecanique{
             this.heros.y<=this.monstre.y+32){
                 console.log("Gagné!");
         }
-        //Penser à une boucle pour la colision avec les bombes
-        else if (this.bombe.x-32<=this.heros.x &&
-            this.heros.x<=this.bombe.x+32 &&
-            this.bombe.y-32<=this.heros.y &&
-            this.heros.y<=this.bombe.y+32){
-                console.log("perdu!");
+    }
+    
+    collisionsBombs(){
+        for(let i = 0; i< this.bombs.length; i++){
+            if (this.bombs[i].x-32<=this.heros.x &&
+                this.heros.x<=this.bombs[i].x+32 &&
+                this.bombs[i].y-32<=this.heros.y &&
+                this.heros.y<=this.bombs[i].y+32){
+                    console.log("perdu!");
+            }
         }
     }
     
     creaBombes(){
         this.bombs = [];
-        this.bombe = new Bombes ("img/bomb.png", this.canvas.context);
         for(let i=0; i<10; i++){
+            this.bombe = new Bombes ("img/bomb.png", this.canvas.context);
             this.bombs.push(this.bombe);
+
         }
         return this.bombs;
     }
